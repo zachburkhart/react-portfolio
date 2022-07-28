369939dd9e7ee5616152f3272bb8f07e2626fcb2
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-28 18:07:19 -0400

    added Header

--- main/src/components/Header.js
+++ main/src/components/Header.js
@@ -1,6 +1,21 @@
 import React from "react";
-import Navigation from "..Navigation";
+import Navigation from "./Navigation";
 
-function Header() {
-    
+function Header(props) {
+    const { thisPage, changePage } = props;
+    return (
+        <header>
+            <div>
+                <h3>Zach's Portfolio</h3>
+            </div>
+            <div>
+                <Navigation
+                    thisPage={thisPage}
+                    changePage={changePage}>
+                </Navigation>
+            </div>
+        </header>
+    );
 }
+
+export default Header;
